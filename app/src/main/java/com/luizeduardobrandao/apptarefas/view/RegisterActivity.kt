0016146671a0950d7378ec8d035f433596d9ab5f
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-01 18:49:20 -0300

    implementando logica inicial para RegisterActivity e RegisterViewModel

--- app/src/main/java/com/luizeduardobrandao/apptarefas/view/RegisterActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/apptarefas/view/RegisterActivity.kt
@@ -1,21 +1,60 @@
 package com.luizeduardobrandao.apptarefas.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.luizeduardobrandao.apptarefas.R
+import com.luizeduardobrandao.apptarefas.databinding.ActivityRegisterBinding
+import com.luizeduardobrandao.apptarefas.viewmodel.RegisterViewModel
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val viewModel: RegisterViewModel by viewModels()
+    private val binding: ActivityRegisterBinding by lazy {
+        ActivityRegisterBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+
+        supportActionBar?.hide()
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left + binding.main.paddingStart, systemBars.top,
+                systemBars.right + binding.main.paddingEnd, systemBars.bottom
+            )
             insets
         }
+
+        // Eventos
+        binding.buttonSave.setOnClickListener(this)
+
+        observe()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun observe() {
+
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        val email = binding.editEmail.text.toString()
+        val password = binding.editPassword.text.toString()
     }
 }
--- app/src/main/java/com/luizeduardobrandao/apptarefas/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/luizeduardobrandao/apptarefas/viewmodel/RegisterViewModel.kt
@@ -1,4 +1,9 @@
 package com.luizeduardobrandao.apptarefas.viewmodel
 
-class RegisterViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.luizeduardobrandao.apptarefas.service.repository.local.PreferencesManager
+
+class RegisterViewModel(application: Application) : AndroidViewModel(application) {
+    private val preferencesManager = PreferencesManager(application.applicationContext)
 }
