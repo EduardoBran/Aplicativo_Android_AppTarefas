1903860d70bb35e294c6622872700bc4e60797e5
EduardoBran <eduardo.ads1814@gmail.com>
2025-07-01 15:32:19 -0300

    iniciando repositorio

---
+++ app/src/main/java/com/luizeduardobrandao/apptarefas/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.luizeduardobrandao.apptarefas.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
